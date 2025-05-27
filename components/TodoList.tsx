77ce95982ffb82aa9dea62fb89ef76e08ac74005
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-27 21:46:39 +0330

    ZUSTAND

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,12 +1,14 @@
 "use client"
+import { Delete } from 'lucide-react'
 import useTodoStore, { TodoStoreType, TodoType } from '../store/todoStore'
 
 function TodoList() {
+
     const { todos, toggleTodo, removeTodo } = useTodoStore() as TodoStoreType
 
     return (
         <ul className='bg-zinc-800 p-3 rounded my-3'>
-            <h2>TODO LIST :</h2>
+            <h2 className='font-black tracking-wide text-green-600'>TODO LIST :</h2>
             {todos.map((todo: TodoType) => (
                 <li key={todo.id} className='bg-black p-2 rounded space-x-5 my-2 text-xl'>
                     <span
@@ -15,7 +17,7 @@ function TodoList() {
                     >
                         {todo.text}
                     </span>
-                    <button onClick={() => removeTodo(todo.id)}>❌</button>
+                    <button onClick={() => removeTodo(todo.id)}><Delete className='text-red-500 translate-y-1' /></button>
                 </li>
             ))}
         </ul>
