3635106eea68a72dd99e544acbcabd18993348ef
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-28 21:50:19 +0330

    ZUSTAND

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 import useTodoStore from '@/store/todoStore'
 import React, { useState } from 'react'
@@ -5,10 +6,16 @@ import { TodoStoreType } from '@/store/todoStore'
 import { FakeTodos } from '@/data/db'
 import { FakeTodos2 } from '@/data/db'
 import { Trash } from 'lucide-react'
+import useSWR from 'swr'
+
 
 const TodoForm = () => {
     const [text, setText] = useState('')
     const { addTodo } = useTodoStore() as TodoStoreType
+    const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+    const { data, error, isLoading } = useSWR('https://fakestoreapi.com/products', fetcher)
+
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
@@ -28,6 +35,7 @@ const TodoForm = () => {
             <button type="submit" className='px-6 py-2 rounded bg-green-800'>ADD</button>
             <button className='bg-black p-2 rounded mx-1 bg-gradient-to-br from-indigo-900 via-sky-950 to-pink-950 border-2 border-black' onClick={() => addTodo([...FakeTodos])}>ADD FAKE TODOS</button>
             <button className='bg-black p-2 rounded mx-1 bg-gradient-to-br from-green-900 via-emerald-950 to-lime-950 border-2 border-black' onClick={() => addTodo([...FakeTodos2])}>ADD FAKE TODOS 2</button>
+            <button className='bg-black p-2 rounded mx-1 bg-gradient-to-br from-rose-900 via-pink-950 to-red-950 border-2 border-black' onClick={() => addTodo([...data])}>ADD FAKE DATA</button>
             <button className='p-2 border-2 border-black rounded mx-1  bg-red-950 inline-flex items-center translate-y-1.5 gap-2' onClick={() => addTodo([])}><Trash />CLEAR TODOS</button>
         </form>
     )
