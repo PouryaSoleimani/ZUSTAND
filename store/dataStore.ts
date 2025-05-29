a470e6a8bacfeafda5742a4ecc8b0a760d96abca
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-29 22:14:49 +0330

    TIC-TAC-TOE

--- store/dataStore.ts
+++ store/dataStore.ts
@@ -1,16 +1,18 @@
 import { create } from "zustand";
 
-
-interface DataType { id: number, title: string }
+interface DataType {
+  id: number;
+  title: string;
+}
 
 interface DataStoreState {
-    datas: DataType[];
-    addDatas: (array: DataType[]) => void;
-    deleteDatas: () => void;
+  datas: DataType[];
+  addDatas: (array: DataType[]) => void;
+  deleteDatas: () => void;
 }
 
 export const useDataStore = create<DataStoreState>((set) => ({
-    datas: [],
-    addDatas: (array: DataType[]) => set(() => ({ datas: array })),
-    deleteDatas: () => set(() => ({ datas: [] }))
-}))
+  datas: [],
+  addDatas: (array: DataType[]) => set(() => ({ datas: array })),
+  deleteDatas: () => set(() => ({ datas: [] })),
+}));
