391b6ad43f06c9cc4f85a408c35046d5a2197634
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 00:52:41 +0330

    ZUSTAND

--- app/products-show/page.tsx
+++ app/products-show/page.tsx
@@ -13,7 +13,7 @@ const ProductsShowPage = () => {
     }
 
     function toaster(id: number | string) {
-        toast(`Product ((${id})) Deleted Successfully`,
+        toast(`Product ((${id})) Deleted`,
             { icon: '❌', style: { borderRadius: '4px', background: '#000000', color: '#fff', border: '1px solid #fff', fontSize: '13px' }, }
         );
     }
@@ -25,9 +25,9 @@ const ProductsShowPage = () => {
     return (
         <div className='flex w-screen h-screen flex-col gap-3 bg-black p-10'>
             <h2 className='text-center font-black bg-blue-950 w-fit mx-auto p-3 rounded-lg my-4'>ProductsShowPage</h2>
-            <button onClick={() => addProducts([{ id: crypto.randomUUID().slice(0, 4), title: generateRandomString(5) }])} className='btn w-fit mx-auto btn-success rounded-lg my-3'>Add Product</button>
+            <button onClick={() => addProducts([{ id: crypto.randomUUID().slice(0, 5), title: generateRandomString(5) }])} className='btn w-fit mx-auto btn-success rounded-lg my-3'>Add Product</button>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 place-items-center gap-5 border p-4 border-zinc-800 rounded-lg'>
-                {products?.map((product: SingleProductType) => (
+                {products.length ? products?.map((product: SingleProductType) => (
                     <>
                         <Suspense fallback={<p className='text-3xl font-black text-white'>LOADING</p>}>
                             <div key={product.id} className='card card-border gap-3 bg-zinc-900 py-3 px-10  flex items-center justify-center rounded-lg'>
@@ -36,7 +36,7 @@ const ProductsShowPage = () => {
                             </div>
                         </Suspense>
                     </>
-                ))}
+                )) : <h2 className='text-center w-fit mx-auto'>NO PRODUCTS</h2>}
             </div>
 
         </div >
