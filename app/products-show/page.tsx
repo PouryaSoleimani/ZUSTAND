5327aaf92883178c3467eeb70023d217ad749b2f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 00:21:32 +0330

    ZUSTAND

--- app/products-show/page.tsx
+++ app/products-show/page.tsx
@@ -3,6 +3,7 @@ import useProductsStore from '@/store/productsStore'
 import React from 'react'
 import { SingleProductType, ProductsStoreType } from '@/store/productsStore'
 import { toast } from 'react-hot-toast'
+import { Trash } from 'lucide-react'
 
 const ProductsShowPage = () => {
 
@@ -24,11 +25,11 @@ const ProductsShowPage = () => {
         <div className='flex flex-col gap-3 w-[90%] mx-auto my-32 p-10'>
             <h2 className='card-title'>ProductsShowPage</h2>
             <button onClick={() => addProducts([{ id: crypto.randomUUID().slice(0, 4), title: generateRandomString(5) }])} className='btn w-fit mx-auto'>Add Product</button>
-            <div className='grid grid-flow-row grid-cols-8 place-items-center gap-3'>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 place-items-center gap-5'>
                 {products?.map((product: SingleProductType) => (
-                    <div key={Math.random()} className='card border w-fit gap-3 bg-zinc-950 p-3 flex items-center justify-center'>
+                    <div key={Math.random()} className='card card-border gap-3 bg-zinc-900 p-3 flex px-6 items-center justify-center'>
                         <p className='font-bold'>{product.title.toUpperCase()}</p>
-                        <button onClick={() => { deleteProducts(product.id); console.log("DELETE", product.id); toaster(product.id) }} className='btn btn-error w-fit'>Delete Products</button>
+                        <button onClick={() => { deleteProducts(product.id); console.log("DELETE", product.id); toaster(product.id) }} className='btn btn-error w-fit flex text-sm text-neutral-800 gap-2 whitespace-nowrap tracking-tighter'><Trash className='w-4 h-4' /> Delete </button>
                     </div>
                 ))}
             </div>
