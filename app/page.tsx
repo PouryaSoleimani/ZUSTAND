8edd3d3fb12c4bcfc81e1586a8a7b47039e92a06
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 18:09:54 +0330

    ZUSTAND___AUTH

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5 w-[80%] mx-auto justify-center mt-20 font-[family-name:var(--font-geist-sans)] border p-4 border-zinc-800 rounded-xl">
+    <div className="flex flex-col gap-5 w-[80%] mx-auto justify-center mt-20 font-[family-name:var(--font-geist-sans)] border p-4 border-zinc-800 rounded-xl bg-black">
       <h1 className="bg-green-700 text-3xl font-bold p-4 rounded">ZUSTAND TRAINING</h1>
       <Counter />
       <TodoList />
