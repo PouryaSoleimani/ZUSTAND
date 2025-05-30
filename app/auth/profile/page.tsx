1cfde611e4ad2d62bf6ac1cc08361c0e3a386e70
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 21:44:35 +0330

    ZUSTAND___AUTH

--- app/auth/profile/page.tsx
+++ app/auth/profile/page.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = () => {
     return (
-        <div>ProfilePage</div>
+        <div>
+            <div className="card card-side bg-base-100 shadow-sm">
+                <figure>
+                    <Image src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" alt="Profile__Pic" width={500} height={500} />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
