adee8c1fa2c0cac9fa78ea42b330546c1f7f8c1e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 18:00:33 +0330

    ZUSTAND___AUTH

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
         <div className='w-[90vw] h-[90vh] flex flex-col justify-center items-center mx-auto bg-black my-10 rounded-xl'>
             <h2 className='bg-pink-500 p-5  rounded-xl my-10 btn-xl text-black font-bold tracking-tighter text-3xl'>LOGIN</h2>
             <form className='mx-auto w-fit flex flex-col gap-5 border-2 border-zinc-500 p-10 rounded-3xl' onSubmit={submitHandler}>
-                <input type="text" placeholder='name' className='w-[300px] h-[40px] rounded-lg text-xl font-semibold  bg-white text-black px-2 py-6' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder='name' className='w-[300px] h-[40px] rounded-lg text-xl  bg-white text-black font-black px-2 py-6' value={name} onChange={(e) => setName(e.target.value)} />
                 <button className='w-[300px] h-[40px] rounded-xl text-white text-xl py-6 btn btn-soft btn-success' type='submit'>Login</button>
             </form>
         </div>
