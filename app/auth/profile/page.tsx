4590aae230a29f136b5e28f54685f78d8a1e1508
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 21:46:54 +0330

    ZUSTAND___AUTH

--- app/auth/profile/page.tsx
+++ app/auth/profile/page.tsx
@@ -1,18 +1,20 @@
+"use client"
+import { LogOut } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = () => {
     return (
-        <div>
+        <div className='w-screen h-screen bg-black flex items-center justify-center'>
             <div className="card card-side bg-base-100 shadow-sm">
                 <figure>
                     <Image src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" alt="Profile__Pic" width={500} height={500} />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
+                    <h2 className="card-title">👋 HELLO</h2>
+                    <p>WELCOME TO PANEL</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+                        <button className="btn btn-error mt-6 btn-sm"><LogOut /> LOGOUT </button>
                     </div>
                 </div>
             </div>
--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
-        port: "",
-        pathname: "/**",
-        search: "",
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+        port: '',
+        pathname: '/**',
+        search: '',
       },
     ],
   },
