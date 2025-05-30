944baeae8c8a81cb81ce809144a1b12843544388
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-30 15:04:03 +0330

    ZUSTAND

--- app/users-fetch-show/page.tsx
+++ app/users-fetch-show/page.tsx
@@ -6,7 +6,7 @@ const UsersFetchShowPage = () => {
     const { users, setUsers, clearUsers } = UseUsersFetchStore()
 
     return (
-        <section className='container mx-auto p-4 my-10 w-11/12 rounded'> 
+        <section className='mockup-window border border-base-300 w-11/12 mx-auto my-10 p-10'> 
             <div>UsersFetchShowPage</div>
             <div>
                 <button className='btn btn-soft btn-success btn-md mx-1 my-2' onClick={() => setUsers()}>SET USERS</button>
@@ -15,8 +15,8 @@ const UsersFetchShowPage = () => {
             <div className='card card-border card-xl'>
                 {users.length ? (
                     users.map((user) => (
-                        <div key={user.id}>
-                            <h2>{user.id} . {user.username.slice(0,15).toUpperCase()}</h2>
+                        <div key={user.id} className='p-1'>
+                            <h2 className='bg-zinc-800 m-1 w-fit p-2 rounded-lg'>{user.id} . {user.username.slice(0,15).toUpperCase()}</h2>
                         </div>
                     ))
                 ) : <h2>No users</h2>}
