69265eec07ef9144a4ec0d478fd25bdf473e5dc5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-01 22:58:30 +0330

    ZUSTAND___AUTH

---
+++ store/ageStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+type UseAgeStoreType = {
+  age: number;
+  setAge: (age: number) => void;
+};
+
+const useAgeStore = create<UseAgeStoreType>()(
+  persist(
+    (set) => ({
+      age: 0,
+      setAge: (age) => set({ age }),
+    }),
+    {
+      name: 'age-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+export default useAgeStore;
