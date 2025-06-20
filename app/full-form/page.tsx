b4c0dad64822e5780e13ff0a74542274b85a7b03
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-20 22:55:32 +0330

    ZUSTAND___AUTH

--- app/full-form/page.tsx
+++ app/full-form/page.tsx
@@ -17,7 +17,6 @@ const FullFormPage = () => {
         setGram(Gram.toUpperCase())
         setMaterial(Material.toUpperCase())
         SetColor(''); SetGram(''); SetMaterial('')
-        location.reload()
     }
     return (
         <div className='w-screen h-screen flex flex-col items-center justify-center'>
